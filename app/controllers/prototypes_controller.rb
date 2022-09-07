b35b4ff5588e4844f54eab48f977a871cb49ce28
pi0723 <coco_mel.0723@icloud.com>
2022-09-07 16:06:01 +0900

    ProtoSpace挙動確認 #1の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -29,6 +28,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id  
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -51,11 +53,4 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
                               .merge(user_id: current_user.id)
   end
-
-  def move_to_index
-    unless user_signed_in? && current_user.id == @prototype.user
-      redirect_to action: :index
-    end
-  end
-
 end
